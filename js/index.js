edc28dcddc7a7a8c771c4c485af3da3672fdb485
Matheus Molina <matheusmolina2@hotmail.com>
2021-07-24 02:00:01 -0300

    muitas alteracoes

--- js/index.js
+++ js/index.js
@@ -29,26 +29,46 @@ function updateBodyDiv() {
 function addQueryParameter() {
   let lista = document.getElementById("listaParametros");
   let li = document.createElement("li");
+  li.className = "row";
+  let divCheckbox = document.createElement("div");
+  divCheckbox.className = "col-md-1";
+  let divInput = document.createElement("div")
+  divInput.className = "col";
+  let divDelete = document.createElement("div");
+  divDelete.className = "col-md-3";
   let parameter = createInput("Parameter")
   let value = createInput("Value")
   let checkbox = createCheckbox()
   checkbox.checked = true;
   let button = createButton(`deleteParamButton${lista.childElementCount + 1}`);
   button.onclick = () => {deleteItem(button.id);}
-  addChildElement(li,[parameter, value, checkbox, button])
+  addChildElement(divCheckbox, [checkbox]);
+  addChildElement(divDelete, [button]);
+  addChildElement(divInput, [parameter, value])
+  addChildElement(li,[divCheckbox, divInput, divDelete])
   lista.appendChild(li);
 }
 
 function addHeader() {
   let lista = document.getElementById("listaHeaders");
   let li = document.createElement("li");
+  li.className = "row";
+  let divCheckbox = document.createElement("div");
+  divCheckbox.className = "col-md-1";
+  let divInput = document.createElement("div")
+  divInput.className = "col";
+  let divDelete = document.createElement("div");
+  divDelete.className = "col-md-3";
   let header = createInput("Header")
   let value = createInput("Value")
   let checkbox = createCheckbox()
-  let button = createButton(`deleteHeaderButton${lista.childElementCount + 1}`);
   checkbox.checked = true;
+  let button = createButton(`deleteHeaderButton${lista.childElementCount + 1}`);
   button.onclick = () => {deleteItem(button.id);}
-  addChildElement(li,[header, value, checkbox, button])
+  addChildElement(divCheckbox, [checkbox]);
+  addChildElement(divDelete, [button]);
+  addChildElement(divInput, [header, value])
+  addChildElement(li,[divCheckbox, divInput, divDelete])
   lista.appendChild(li);
 }
 
@@ -57,6 +77,7 @@ function createInput(name){
   input.type = "text";
   input.name = name;
   input.placeholder = name;
+  input.className = "form-control form-control-sm"
   return input
 }
 
@@ -80,7 +101,7 @@ function createButton(buttonID){
 }
 
 function deleteItem(id) {
-  document.getElementById(id).parentElement.remove();
+  document.getElementById(id).parentElement.parentElement.remove();
 }
 
 
@@ -97,21 +118,32 @@ function getQueryParams(){
 }
 
 function getHeaderValues(){
-
+  let lista = Array.from(document.querySelectorAll("#listaHeaders > li"));
+  lista = getValidInputValues(lista)
+  if (lista.length == 0) return "";
+  let queryHeader = ""
+  lista.forEach((header)=>{
+    queryHeader = queryHeader + `\n -H \"${header.name}: ${header.value}\"`
+  });
+  return queryHeader
 }
 //REFATORAR
 function getValidInputValues(elementsArr){
   return elementsArr.filter((element)=>{
-    let name = element.querySelector("input:nth-child(1)").value
-    let value = element.querySelector("input:nth-child(2)").value
+    let name = element.querySelector("div:nth-child(2) > input[type='text']:nth-child(1)").value
+    let value = element.querySelector("div:nth-child(2) > input[type='text']:nth-child(2)").value
     if(name === "" ||  value === ""){
-      return false
+      return false;
     }else {
-      return true
+      let checkbox = element.querySelector("input[type='checkbox']")
+      if(checkbox.checked){
+        return true;
+      }
+      return false;
     }
   }).map((element) => {
-    let name = element.querySelector("input:nth-child(1)").value
-    let value = element.querySelector("input:nth-child(2)").value
+    let name = element.querySelector("div:nth-child(2) > input[type='text']:nth-child(1)").value
+    let value = element.querySelector("div:nth-child(2) > input[type='text']:nth-child(2)").value
     return {name, value}
   });
 
@@ -125,10 +157,21 @@ function getHTTPMethod(){
 }
 
 function generateCURL(){
-  if(getURL() == "") return
+  if(!validaURL()) return;
   let generatedCURL = "cURL "
   if(getHTTPMethod() === "POST") generatedCURL = generatedCURL + "-X POST ";
   generatedCURL = generatedCURL + getURL();
   generatedCURL = generatedCURL + getQueryParams();
+  generatedCURL = generatedCURL + getHeaderValues();
   document.getElementById("GeneratedCurl").value = generatedCURL;
+}
+
+function validaURL(){
+  if(getURL() == ""){
+    document.getElementById("url").style.background = "#e66f56"
+    return 
+  }else {
+    document.getElementById("url").style.background = "white"
+    return true
+  }
 }
